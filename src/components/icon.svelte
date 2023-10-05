14cb8278ea1a55b6fdea465d2a791258c38ae13c
alan-cash <contactalanvalencia@gmail.com>
2023-10-05 23:09:50 +0200

    feat: :triangular_flag_on_post: Added reusable icon component for improved UI consistency and modularity

---
+++ src/components/icon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import icons from '@lib/icons.json'
+
+  export let name: keyof typeof icons
+  export let size: string = '1rem'
+  export let focusable: string | number | null | undefined
+
+  const displayIcon = icons[name]
+</script>
+
+<svg
+  class={$$props.class}
+  {focusable}
+  width={size}
+  height={size}
+  viewBox="0 0 {displayIcon.box} {displayIcon.box}"
+>
+  <!--eslint-disable-next-line svelte/no-at-html-tags -->
+  {@html displayIcon.svg}
+</svg>
