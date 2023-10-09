0319f6fb7b9b50acc56139f6756c4f24fc48d98e
alan-cash <contactalanvalencia@gmail.com>
2023-10-09 21:35:30 +0200

    refactor: :art:

---
+++ src/components/carousel.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  let slider: HTMLDivElement | null = null
+  let isSliderDragging = false
+  let xSliderScroll = 0
+
+  const dragStart = (): boolean => (isSliderDragging = true)
+
+  const dragMove = (e: MouseEvent): void => {
+    if (!isSliderDragging || slider === null) return
+
+    slider.scrollLeft -= e.movementX
+    xSliderScroll = slider.scrollLeft
+  }
+
+  const dragStop = (): boolean => (isSliderDragging = false)
+</script>
+
+<div
+  role="slider"
+  tabindex="0"
+  aria-valuenow={xSliderScroll}
+  class="select-none flex overflow-x-scroll my-4"
+  on:mousedown={dragStart}
+  on:mousemove={dragMove}
+  on:mouseup={dragStop}
+  on:mouseleave={dragStop}
+  bind:this={slider}
+>
+  <div
+    class="flex items-center flex-shrink-0 gap-2"
+    style:cursor={isSliderDragging ? 'grabbing' : 'grab'}
+  >
+    <slot />
+  </div>
+</div>
---
+++ src/components/link.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { HTMLAttributeAnchorTarget } from 'svelte/elements'
+
+  export let to: string
+  export let text: string
+  export let textHoverColor: string = 'current'
+  export let color: 'gray' | 'white' | 'black' = 'gray'
+  export let target: HTMLAttributeAnchorTarget = '_self'
+  export let responsive: boolean = false
+</script>
+
+<a
+  href={to}
+  class="px-6 py-2 text-center font-semibold rounded-full border-2 border-[--anchor-color] text-[--anchor-color] hover:text-[--text-hover-color] hover:bg-[--anchor-color] hover:border-transparent active:bg-[--anchor-color] active:border-transparent active:text-[--text-hover-color] select-none cursor-pointer {responsive &&
+    'md:text-2xl md:px-10 md:py-4'} capitalize {$$props.class}"
+  style:--text-hover-color={textHoverColor}
+  style:--anchor-color={color === 'gray' ? '#2F3032' : color}
+  {target}
+>
+  {text}
+</a>
