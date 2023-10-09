f0a79928ab14ae723326f36fd9b7026aca2ce72e
alan-cash <contactalanvalencia@gmail.com>
2023-10-09 21:38:10 +0200

    refactor: :recycle:

--- src/components/icon.svelte
+++ src/components/icon.svelte
@@ -4,14 +4,13 @@
   export let name: keyof typeof icons
   export let size: string = '1rem'
   export let color: string | null | undefined = 'currentColor'
-  export let className: string | null | undefined
 
   const displayIcon = icons[name]
 </script>
 
 <svg
   {color}
-  class={className}
+  class={$$props.class}
   width={size}
   height={size}
   viewBox="0 0 {displayIcon.box} {displayIcon.box}"
