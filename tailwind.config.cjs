20239fef9c9cbf394bdbec82d589e562e87f74f2
alan-cash <contactalanvalencia@gmail.com>
2023-10-09 21:28:57 +0200

    feat: :art:

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        serif: ['"EB Garamond"', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: []
 }
