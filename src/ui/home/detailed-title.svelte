5e418c658db57c048c27f748da4ff093f04e5fdd
imalanvalencia <contacto.alanvalencia@gmail.com>
2023-10-13 20:15:20 +0200

    feat: :sparkles: Developed detailed (big) title component and favorites projects component

---
+++ src/ui/home/detailed-title.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Icon from '@components/icon.svelte'
+
+  export let title: string
+  export let direction: 'vertical' | 'horizontal' = 'vertical'
+</script>
+
+<section
+  class="w-full md:min-h-[32rem] flex justify-center items-center px-8 py-16 text-orange-400 gap-8 {$$props.class}"
+  class:flex-col={direction === 'vertical'}
+  class:flex-row={direction === 'horizontal'}
+>
+  <h2 class="w-min text-center font-serif font-bold small-caps">{title}</h2>
+  <Icon
+    name="arrow"
+    size="2rem"
+    class={direction === 'vertical' && 'rotate-90'}
+  />
+
+  <style>
+    h2 {
+      font-size: clamp(3.625rem, 1.425rem + 11vw, 14.625rem);
+      line-height: clamp(3.125rem, 0.625rem + 12.5vw, 15.625rem);
+    }
+  </style>
+</section>
