b8bd702147b7aca9903633cd66a34ea7a8e97186
imalanvalencia <contacto.alanvalencia@gmail.com>
2023-10-21 23:16:37 +0200

    feat: :art: Developed new animations for Tailwind CSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,31 @@ module.exports = {
       fontFamily: {
         sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
         serif: ['"EB Garamond"', ...defaultTheme.fontFamily.serif]
+      },
+
+      animation: {
+        'marquee-slow': 'marquee 30s linear infinite',
+        'marquee-fast': 'marquee 5s linear infinite',
+        'marquee-regular': 'marquee 15s linear infinite',
+        change: 'change 3.5s linear',
+        'to-up': 'to-up 2s linear infinite'
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' }
+        },
+
+        change: {
+          '25%': {
+            transform: 'translateY(0)',
+            opacity: 1
+          },
+          '90%': {
+            transform: 'translateY(-20px)',
+            opacity: 0
+          }
+        }
       }
     }
   },
