007bbfbdb68f53f8e428b56fabecbd0b40a1481f
imalanvalencia <contacto.alanvalencia@gmail.com>
2023-11-09 23:50:03 +0100

    fix: :bug: Correcting errors with text-stroke property

--- src/components/text-stroke.svelte
+++ src/components/text-stroke.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let upText = 'Frontend'
+  export let upText = 'Fullstack'
   export let downText = 'Developer'
 </script>
 
@@ -7,11 +7,11 @@
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 680 232"
   class="text-8xl tracking-[0.005rem] fill-white font-sans font-extrabold uppercase {$$props.class}">
-
   <g>
-    <rect fill="#8c7b5a" width="50%" height="50%" x="25%" y="25%"/>
+    <rect fill="#8c7b5a" width="50%" height="50%" x="25%" y="25%" />
     <text
-      transform="translate(50.72 99.2)"
+      x="-19"
+      transform="translate(50 99.2)"
       translate="no"
       style="
         stroke-linecap: round;
@@ -19,11 +19,11 @@
       "
       class="stroke-[#8c7b5a] stroke-[56px] stroke">
       {upText}
-      <tspan x="-25.82" y="96">{downText}</tspan>
+      <tspan x="-25" y="96">{downText}</tspan>
     </text>
-    <text transform="translate(50.72 99.2)" translate="no">
+    <text x="-19" transform="translate(50 99.2)" translate="no">
       {upText}
-      <tspan x="-25.82" y="96">{downText}</tspan>
+      <tspan x="-25" y="96">{downText}</tspan>
     </text>
   </g>
 </svg>
