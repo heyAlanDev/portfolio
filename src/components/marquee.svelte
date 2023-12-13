e008c114284547fdc22bfc4692b943d85c50c714
imalanvalencia <contacto.alanvalencia@gmail.com>
2023-12-13 23:40:03 +0100

    style: :art: Introduced dark mode styling for the about section

--- src/components/marquee.svelte
+++ src/components/marquee.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-  export let bgColor = '#BC9CE7'
   export let time: 'fast' | 'regular' | 'slow' = 'fast'
   export let reverse = false
 </script>
 
 <article
-  class="rounded-2xl p-4 min-w-full bg-[--bg-color] text-current font-bold select-none {$$props.class}"
-  style:--bg-color={bgColor}
+  class="rounded-2xl p-4 min-w-full  text-current font-bold select-none {$$props.class}"
 >
   <div
-    class="overflow-hidden whitespace-nowrap relative capitalize self-center will-change-transform before:left-0 after:right-0 before:bg-gradient-to-r after:bg-gradient-to-l from-[--bg-color]"
+    class="overflow-hidden whitespace-nowrap relative capitalize self-center will-change-transform h-full before:left-0 after:right-0 before:bg-gradient-to-r after:bg-gradient-to-l"
   >
     {#each Array(2) as _}
       <div
-        class="inline-flex"
+        class="inline-flex h-full"
         class:animate-marquee-regular={time === 'regular'}
         class:animate-marquee-fast={time === 'fast'}
         class:animate-marquee-slow={time === 'slow'}
@@ -28,7 +26,7 @@
 <style lang="postcss">
   article > div::after,
   article > div::before {
-    @apply absolute top-0 w-1/12 h-full content-[""] z-10;
+    @apply absolute top-0 bottom-0 w-1/12 h-full content-[""] z-10;
   }
 
   article:hover div {
