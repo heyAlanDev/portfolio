72f5d1ca9e5196e04a8e0768ae6800f78adf68c4
imalanvalencia <contacto.alanvalencia@gmail.com>
2023-12-25 23:25:36 +0100

    fix: :bug: Resolved the issue with navbar click toggling navbar state and refactored to an isolated component

--- src/components/menu.svelte
+++ src/components/menu.svelte
@@ -1,25 +1,51 @@
 <script lang="ts">
-  export let isMenuOpen: boolean
-  export let toggleMenuState: (e: Event) => void
+  import { onMount } from 'svelte'
+
+  let navbarElementsToToggle: NodeListOf<HTMLElement>
+  let links: NodeListOf<HTMLAnchorElement>
+
+  export const toggleNavbarState = () => {
+    if (!navbarElementsToToggle) return
+
+    navbarElementsToToggle.forEach(element => {
+      const { navbar } = (element as HTMLElement).dataset
+
+      navbar === 'close'
+        ? ((element as HTMLElement).dataset.navbar = 'open')
+        : ((element as HTMLElement).dataset.navbar = 'close')
+    })
+  }
+
+  onMount(() => {
+    navbarElementsToToggle = document.querySelectorAll('[data-navbar]')
+    navbarElementsToToggle.forEach(element => {
+      element.dataset.navbar = 'close'
+    })
+
+    // idk if this is a good practice
+    links = document.querySelectorAll('nav a')
+    links.forEach(link => {
+      link.addEventListener('click', toggleNavbarState)
+    })
+  })
 </script>
 
 <button
-  class="cursor-pointer relative before:absolute before:inset-1/2 before:-z-10 before:rounded-full before:w-1 before:h-1 before:transition-transform before:duration-300
-  {isMenuOpen && 'before:bg-teal-300 before:scale-[500]'}"
-  on:click={toggleMenuState}
-  data-movil-menu={isMenuOpen}
-  aria-label="Navbar Menu Button"
+  class="cursor-pointer relative group before:absolute before:inset-1/2 before:-z-10 before:rounded-full before:w-1 before:h-1 before:transition-transform before:duration-300 data-[navbar=open]:before:bg-teal-300 data-[navbar=open]:before:scale-[500]"
+  data-navbar="close"
+  on:click={() => {
+    toggleNavbarState()
+    document.body.classList.toggle('overflow-hidden')
+  }}
 >
-  {#each [1, 2, 3] as index}
-    <div
-      class="h-1 bg-current m-1 transition-transform duration-500ms"
-      class:-translate-x-1={isMenuOpen}
-      class:w-8={index !== 2 ?? !isMenuOpen}
-      class:w-0={index === 2 && isMenuOpen}
-      class:rotate-45={index === 1 && isMenuOpen}
-      class:-rotate-45={index === 3 && isMenuOpen}
-      class:translate-y-2={index === 1 && isMenuOpen}
-      class:-translate-y-2={index === 3 && isMenuOpen}
-    />
-  {/each}
+  <div
+    class="h-1 w-8 bg-current m-1 transition-transform duration-500ms group-data-[navbar=open]:rotate-45 group-data-[navbar=open]:-translate-x-1 group-data-[navbar=open]:translate-y-2"
+  />
+  <div
+    class="h-1 w-8 bg-current m-1 transition-transform duration-500ms group-data-[navbar=open]:-translate-x-1 group-data-[navbar=open]:w-0"
+  />
+
+  <div
+    class="h-1 w-8 bg-current m-1 transition-transform duration-500ms group-data-[navbar=open]:-rotate-45 group-data-[navbar=open]:-translate-x-1 group-data-[navbar=open]:-translate-y-2"
+  />
 </button>
